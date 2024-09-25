3a1551e2f1ef4ca024ede25d7f74da63979d998c
farfromrefug <dev@akylas.fr>
2024-09-25 10:57:15 +0200

    fix: window override was not working

--- src/additween/AdditiveTweening.ts
+++ src/additween/AdditiveTweening.ts
@@ -1,6 +1,25 @@
 import { PlainObjectReducer } from "./PlainObjectReducer";
 import type { IStateReducer } from "./PlainObjectReducer";
+import { time } from '@nativescript/core/profiling';
 
+if (!global.window) {
+    global.window = {
+        requestAnimationFrame,
+        cancelAnimationFrame,
+        performance: {
+            now: time
+        }
+    } as any;
+} else if (!global.window.requestAnimationFrame) {
+    global.window.requestAnimationFrame = requestAnimationFrame;
+    global.window.cancelAnimationFrame = cancelAnimationFrame;
+    if (!global.window.performance) {
+        //@ts-ignore
+        global.window.performance = {
+            now: time
+        };
+    }
+}
 function noop() {}
 
 function identity(t: number): number {
--- src/additween/index.ts
+++ src/additween/index.ts
@@ -2,26 +2,6 @@ import type { IStateReducer } from "./PlainObjectReducer";
 import type { EasingFunction } from "./AdditiveTweening";
 import { PlainObjectReducer } from "./PlainObjectReducer";
 import { AdditiveTweening } from "./AdditiveTweening";
-import { time } from '@nativescript/core/profiling';
-
-if (!global.window) {
-    global.window = {
-        requestAnimationFrame,
-        cancelAnimationFrame,
-        performance: {
-            now: time
-        }
-    } as any;
-} else if (!global.window.requestAnimationFrame) {
-    global.window.requestAnimationFrame = requestAnimationFrame;
-    global.window.cancelAnimationFrame = cancelAnimationFrame;
-    if (!global.window.performance) {
-        //@ts-ignore
-        global.window.performance = {
-            now: time
-        };
-    }
-}
 
 export type { EasingFunction, IStateReducer };
 
