5414e41437fa8f9aca7ce0175a13bad6b32af2d6
farfromrefug <dev@akylas.fr>
2024-09-25 10:30:14 +0200

    readme/doc

--- docs/assets/navigation.js
+++ docs/assets/navigation.js
@@ -1,1 +1,1 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA2XOPw+CMBCH4e/SmajU4L/NQRMnjboZh6M95bRWQg/UGL67nYTC/Hvz3J2+gvHNYiGWWhNThccXoiV7FZHIgTO/KAPOoRt2i0HGD+OzO1ktFrGc1dFf2xwYGPeoS4VFQ5FlLC6gvBYUISWTSYtagfPH1qVVTE/bWPzJPROuHWc0n8aJbFk7A2S36Q0V936roCBIjTf7VeiOZX3+AVtQ/EM5AQAA"
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uOBQApu0wNAgAAAA=="
